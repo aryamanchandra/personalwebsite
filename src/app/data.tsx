49f07e9dc334c0974ddee50432b004cf5063a79c
aryamanchandra <aryamanhere@gmail.com>
2023-12-24 03:12:44 +0530

    work experience section

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -1,37 +1,37 @@
 const projectdata = [
   {
     title: "Monumenta",
-    stack:"App Dev | Full Stack Dev | UI/UX",
+    stack: "App Dev | Full Stack Dev | UI/UX",
     description:
       "Monumenta lets you discover a treasure trove of monument details at your fingertips along with providing you with numerous features and sleek UI.",
   },
   {
     title: "Chat It",
-    stack:"Full Stack Dev | UI/UX",
+    stack: "Full Stack Dev | UI/UX",
     description:
       "A messaging platform where you can initiate conversations with others by simply sharing a link. Letting you connect with the someone seamlessly and securely.",
   },
   {
     title: "Unmask",
-    stack:"Full Stack Dev | ML | UI/UX",
+    stack: "Full Stack Dev | ML | UI/UX",
     description:
       "A surveillance app for taking input from CCTV and providing various analysis like mask detection, face recognition, crowd detection etc.",
   },
   {
     title: "Line",
-    stack:"Full Stack Dev | ML | App Dev",
+    stack: "Full Stack Dev | ML | App Dev",
     description:
       "Line is a road safety app that does behavior tagging, lane detection, distraction detachment to prevent road collisions and inefficient driving.",
   },
   {
     title: "Shuttl",
-    stack:"Full Stack Dev | App Dev",
+    stack: "Full Stack Dev | App Dev",
     description:
       "A dedicated shuttle app for our college providing students with real-time location updates, ensuring seamless access to campus infrastructure and promoting efficient transportation within the campus.",
   },
   {
     title: "Supplify",
-    stack:"Full Stack Dev",
+    stack: "Full Stack Dev",
     description:
       "A blockchain-based supply chain to enhance efficiency by minimizing paperwork, streamlining logistics, and fostering transparency and securing transactions.",
   },
@@ -43,21 +43,35 @@ const workdata = [
     company: "Kommit Techno Legal",
     date: "October 2023 - Present",
     description:
-      "Co-Founder of Polychain Monsters with focus on Product, Design and Community Development in the Web3 space, utilizing transparency, user involvement and a user-centric approach to drive successful product development.",
+      "Built a full stack digital notarization application. Worked on analysing the system flow and work processes visually and then implementing in the frontend UI and backend APIs for seamless notarization and user experience.",
   },
   {
     title: "Motion Design Freelancer",
     company: "Anicut Capital",
     date: "April 2023 - Jun 2023",
     description:
-      "Co-Founder of Polychain Monsters with focus on Product, Design and Community Development in the Web3 space, utilizing transparency, user involvement and a user-centric approach to drive successful product development.",
+      "Created diverse promo videos for the website which involved incorporating external clips, extensive color grading. Worked on creating animated GIFs for more appealing look of the website.",
   },
   {
     title: "Design and Motion Design Intern",
     company: "Halara",
     date: "January 2023 - April 2023",
     description:
-      "Co-Founder of Polychain Monsters with focus on Product, Design and Community Development in the Web3 space, utilizing transparency, user involvement and a user-centric approach to drive successful product development.",
+      "Worked on making of an educational AR/VR/MR comparison video. Analysed promotional content for better blending of creativity and technicality.",
+  },
+  {
+    title: "Chief Advisor",
+    company: "Exun Clan",
+    date: "August 2018 - August 2022",
+    description:
+      "Led creative/technical teams at Exun event(22+ events, 2000+ participants each year). Anchored Exun Talks 2022 hosting professionals from all around the world. Taught courses on Design and Motion Design to 100+ people including organisation of Girls in Tech initiative. Won/participated in 20+ hackathons internationally and nationally.",
+  },
+  {
+    title: "Honorary Chief Advisor",
+    company: "Aeross",
+    date: "August 2018 - August 2022",
+    description:
+      "Won/Participated in 7+ NASA and NSS competitions. Met External Affairs Minister and Vice President of India. Was part of numerous projects like CubeSAT, HAB etc.",
   },
 ];
 
