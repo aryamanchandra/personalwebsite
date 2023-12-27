3bfed6dc3b3fc9dca8e2178d0d1842c3e604ac40
aryamanchandra <aryamanhere@gmail.com>
2023-12-27 23:57:44 +0530

    seo head tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,25 +28,45 @@ export default function RootLayout({
         <title>Aryaman Chandra</title>
         <meta name="description" content="Aryaman Chandra's personal website" />
         <link rel="icon" href="../../public/ac-icon.png" sizes="any" />
+        <link rel="canonical" href="https://aryamanchandra.com/" />
+        <meta name="author" content="Aryaman Chandra" />
+        <meta itemProp="name" content="Aryaman Chandra" />
+        <meta itemProp="url" content="https://aryamanchandra.com/" />
+        <meta
+          itemProp="description"
+          content="Aryaman Chandra is a full stack developer and ui/ux designer."
+        />
+        <meta property="og:title" content="Aryaman Chandra" />
+        <meta property="og:url" content="https://aryamanchandra.com/" />
+        <meta property="og:site_name" content="Aryaman Chandra" />
+        <meta
+          property="og:image"
+          content="https://aryamanchandra.com/_next/image?url=%2Fprofile.webp&w=640&q=75"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:description"
+          content="Aryaman Chandra is a Full Stack developer and UI/UX designer."
+        />
       </head>
       <body className={SFText.className}>
         <DataProvider>
-        <ThemeProvider theme={theme}>
-          <Box
-            sx={{
-              width: "100%",
-              maxWidth: "50rem",
-              marginX: "auto",
-              padding: "30px",
-              '@media (max-width: 600px)': {
-                maxWidth: "100%", 
-              },
-            }}
-          >
-            <Navbar />
-            {children}
-          </Box>
-        </ThemeProvider>
+          <ThemeProvider theme={theme}>
+            <Box
+              sx={{
+                width: "100%",
+                maxWidth: "50rem",
+                marginX: "auto",
+                padding: "30px",
+                "@media (max-width: 600px)": {
+                  maxWidth: "100%",
+                },
+              }}
+            >
+              <Navbar />
+              {children}
+            </Box>
+          </ThemeProvider>
         </DataProvider>
       </body>
     </html>
